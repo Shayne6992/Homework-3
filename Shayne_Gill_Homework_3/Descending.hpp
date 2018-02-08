c157aa79010960d8a2918ea2ebf668f21943c255
Shayne <Shayne6992@gmail.com>
2018-02-08 15:00:27 -0600

    Need to incorporate ascending and descending with main.

---
+++ Shayne_Gill_Homework_3/Descending.hpp
@@ -0,0 +1,14 @@
+//
+//  Descending.hpp
+//  Shayne_Gill_Homework_3
+//
+//  Created by Shayne Gill on 2/8/18.
+//  Copyright © 2018 Shayne Gill. All rights reserved.
+//
+
+#ifndef Descending_hpp
+#define Descending_hpp
+
+#include <stdio.h>
+
+#endif /* Descending_hpp */
